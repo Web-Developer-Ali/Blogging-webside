5ffb14a0268a471dd01749fd613948b5f9f9618d
Web-Developer-Ali <148641299+Web-Developer-Ali@users.noreply.github.com>
2024-07-24 08:47:12 +0500

    make cookies secure

--- Back-end/utils/jwtToken.js
+++ Back-end/utils/jwtToken.js
@@ -1,12 +1,12 @@
-export const jwtToken =(user, message, statusCode, res) => {
+export const jwtToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   res
     .status(statusCode)
     .cookie("User", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly:true
+      expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV, // Secure cookies in production
+      sameSite: 'None' // Necessary for cross-site cookie sharing
     })
     .json({
       success: true,
